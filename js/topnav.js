002df217282c679a087381b48e04ac16647c8bc5
Ksites <158746568+gprogramming360@users.noreply.github.com>
2024-06-19 07:39:31 +0100

    update topnav.js

--- js/topnav.js
+++ js/topnav.js
@@ -8,7 +8,7 @@ document.write(c);
 var d="<img onclick='opensidebar()' src='../img/bars.svg' class='active'  id='nodisplay' alt='Menu'>";
 document.write(d);
 //Close Sidebar
-var e="<img onclick='closesidebar()' src='../img/xmark.svg' class='active'  id='displayit' alt='Exit Menu'></div>";
+var e="<img onclick='closesidebar()' src='../img/close_white_24dp.svg' class='active'  id='displayit' alt='Exit Menu'></div>";
 document.write(e);
 //End Topnav
 
@@ -16,19 +16,19 @@ document.write(e);
 var f="<nav id='openav'><h3><strong>"+site_name+"</strong></h3><div class='block-1'>";
 document.write(f);
 //Links
-var g="<span><img src='../img/house-chimney-window.svg' alt='Home icon'><a href='../index.html' rel='tags' alt='Home'>Home</a></span></br>";
+var g="<span><img src='../img/house-home_white.svg' alt='Home icon'><a href='../index.html' rel='tags' alt='Home'>Home</a></span></br>";
 document.write(g);
 
-var h="<span><img src='../img/book-open-reader.svg' alt='Home icon'><a href='#manga' rel='tags' alt='Manga'>Manga</a></span></br>";
+var h="<span><img src='../img/book-reader-solid.svg' alt='Home icon'><a href='#manga' rel='tags' alt='Manga'>Manga</a></span></br>";
 document.write(h);
 
-var i="<span><img src='../img/play.svg' alt='Home icon'><a href='#seasons' rel='tags' alt='Episode'>Episodes</a></span></br>";
+var i="<span><img src='../img/play-solid.svg' alt='Home icon'><a href='#seasons' rel='tags' alt='Episode'>Episodes</a></span></br>";
 document.write(i);
 
-var j="<span><img src='../img/infinity.svg' alt='Home icon'><a href='#footer' rel='tags' alt='About us'>About Us</a></span></br>";
+var j="<span><img src='../img/people_white_24dp.svg' alt='Home icon'><a href='#footer' rel='tags' alt='About us'>About Us</a></span></br>";
 document.write(j);
 
-var jk="<span><img src='../img/open-in-new-window.svg' alt='Watchfilm'><a href='https://watchfilm.xyz' rel='tags' alt='Watchfilm'>Other Animes</a></span></br>";
+var jk="<span><img src='../img/launch_white_24dp.svg' alt='Watchfilm'><a href='https://watchfilm.xyz' rel='tags' alt='Watchfilm'>Other Animes</a></span></br>";
 document.write(jk);
 //End Links
 var k="</div></nav><div id='closenav' onclick='closesidebar()'></div>";
