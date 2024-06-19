b3e34b670594db57754edabd1b895d3ac3303677
Ksites <158746568+gprogramming360@users.noreply.github.com>
2024-06-19 10:17:09 +0100

    update vignetteads.js

--- js/vignetteads.js
+++ js/vignetteads.js
@@ -1,2 +1,2 @@
-var ads2=" ";
+var ads2="<script>(function(d,z,s){s.src='https://'+d+'/401/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('aistekso.net',7628628,document.createElement('script'))</script>";
 document.write(ads2);
