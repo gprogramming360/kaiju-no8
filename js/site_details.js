ccf221a07c130d7405005a06297789afd452ca7c
Ksites <158746568+gprogramming360@users.noreply.github.com>
2024-06-19 08:09:21 +0100

    update site_details.js

--- js/site_details.js
+++ js/site_details.js
@@ -4,5 +4,5 @@ var site_logo="../img/logo.webp";
 var site_image="../img/background-1.webp";
 var site_url="https://kaiju-no8.watchfilm.xyz";
 //ONLY FOR MANGA KEYWORDS
-var keywords="kaiju no 8, kaiju no 8 anime series, kaiju no 8 manga online, read kaiju no 8 manga online, read kaiju no 8 latest chapter online, download kaiju no 8 movies, download kaiju no 8 anime, download kaiju no 8 movies HD, Download kaiju no 8 latest anime series, kaiju no 8 background story, kaiju no 8 season 1, kaiju no 8 season 2,kaiju no 8 season 3,kaiju no 8 season 4,kaiju no 8 season 5, Monster #8";
+var keywords="kaiju no 8 anime series, kaiju no 8 manga online, read kaiju no 8 manga online, read kaiju no 8 latest chapter, download kaiju no 8 movies, download kaiju no 8 anime, download kaiju no 8 movies, Download kaiju no 8 latest anime series, kaiju no 8 background story, kaiju no 8 season 1, kaiju no 8 season 2,kaiju no 8 season 3,kaiju no 8 season 4,kaiju no 8 season 5, Monster #8";
 //END ONLY FOR MANGA KEYWORDS
